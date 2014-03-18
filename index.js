c63dabf7058837f150216de3a67d58a2eb1a7cfe
Spencer Spiegel <spencer.spiegel@gmail.com>
2014-03-18 10:05:07 -0600

    updated passport

--- index.js
+++ index.js
@@ -95,28 +95,28 @@ if(global.process.env.MONGOHQ_URL){
 
 
 
-app.configure = function configure(nconf, next) {
-    // Async method run on startup.
-    next(null);
-};
+// app.configure = function configure(nconf, next) {
+//     // Async method run on startup.
+//     next(null);
+// };
 
 
-app.requestStart = function requestStart(server) {
-    // Run before most express middleware has been registered.
-};
+// app.requestStart = function requestStart(server) {
+//     // Run before most express middleware has been registered.
+// };
 
 
-app.requestBeforeRoute = function requestBeforeRoute(server) {
-    // Run before any routes have been added.
-    server.use(passport.initialize());
-    server.use(passport.session());
-    // server.use(flash());
-    // server.use(auth.injectUser);
-};
+// app.requestBeforeRoute = function requestBeforeRoute(server) {
+//     // Run before any routes have been added.
+//     server.use(passport.initialize());
+//     server.use(passport.session());
+//     // server.use(flash());
+//     // server.use(auth.injectUser);
+// };
 
-app.requestAfterRoute = function requestAfterRoute(server) {
-    // Run after all routes have been added.
-};
+// app.requestAfterRoute = function requestAfterRoute(server) {
+//     // Run after all routes have been added.
+// };
 
 
 if (require.main === module) {
--- models/admin.js
+++ models/admin.js
@@ -84,6 +84,31 @@ passport.use(new LocalStrategy(
 ));
 
 
+app.configure = function configure(nconf, next) {
+    // Async method run on startup.
+    next(null);
+};
+
+
+app.requestStart = function requestStart(server) {
+    // Run before most express middleware has been registered.
+};
+
+
+app.requestBeforeRoute = function requestBeforeRoute(server) {
+    // Run before any routes have been added.
+    server.use(passport.initialize());
+    server.use(passport.session());
+    // server.use(flash());
+    // server.use(auth.injectUser);
+};
+
+app.requestAfterRoute = function requestAfterRoute(server) {
+    // Run after all routes have been added.
+};
+
+
+
 
 
 
